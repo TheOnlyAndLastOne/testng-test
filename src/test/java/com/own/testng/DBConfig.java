3365e36e71186eddcfd0f3f665b2e9c359042897
zhi zhao <zhi.zhao@taotu-partner.com>
2019-05-18 12:40:58 +0800

    init project

---
+++ src/test/java/com/own/testng/DBConfig.java
@@ -0,0 +1,32 @@
+package com.own.testng;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
+/**
+ * @author zhi.zhao
+ * @date 5/18/2019 12:29 PM
+ */
+public class DBConfig {
+    @BeforeSuite()
+    public void beforeSuite() {
+        System.out.println("@BeforeSuite");
+    }
+
+    @AfterSuite()
+    public void afterSuite() {
+        System.out.println("@AfterSuite");
+    }
+
+    @BeforeTest()
+    public void beforeTest() {
+        System.out.println("@BeforeTest");
+    }
+
+    @AfterTest()
+    public void afterTest() {
+        System.out.println("@AfterTest");
+    }
+}
---
+++ src/test/java/com/own/testng/TestDBConnection.java
@@ -0,0 +1,21 @@
+package com.own.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author zhi.zhao
+ * @date 5/18/2019 12:12 PM
+ */
+public class TestDBConnection {
+
+    @Test
+    public void runOtherTest1() {
+        System.out.println("@Test - runOtherTest1");
+    }
+
+    @Test
+    public void runOtherTest2() {
+        System.out.println("@Test - runOtherTest2");
+    }
+
+}
